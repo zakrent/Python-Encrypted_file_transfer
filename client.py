f8544f648d39037e668d725055796100619e499c
zakrent <szymon.zakrent@gmail.com>
2017-01-14 21:48:18 +0100

    Added basic file transfer system

---
+++ client.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+import socket
+
+def handler(s):
+	data = s.recv(2048).decode('UTF-8')
+	if data == "ASF":
+		s.send("RDY".encode('UTF-8'))
+		data = s.recv(2048).decode('UTF-8')
+		with open(data, 'wb') as f:
+			s.send("RDY".encode('UTF-8'))
+			filesize = int(s.recv(2048).decode('UTF-8'))
+			recived = 0
+			while filesize - recived > 0:
+				data = s.recv(2048)
+				recived += len(data)
+				f.write(data)
+	elif data == "ALS":
+		while data != "END":
+			s.send("RDY".encode('UTF-8'))
+			data = s.recv(2048).decode('UTF-8')
+			if data != "END":
+				print(data)
+
+s = socket.socket()
+s.connect(('10.0.0.3',5000))
+print("Connected")
+data = s.recv(2048).decode('UTF-8')
+if data == "AUT":
+	passw = input("Password ->")
+	s.send(passw.encode('UTF-8'))
+	data = s.recv(2048).decode('UTF-8')
+	if data == "ACK":
+		while True:
+			command = input("->")
+			s.send(command.encode('UTF-8'))
+			handler(s)
+
+s.close()
---
+++ server.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python3
+
+import os
+import socket
+from threading import Thread
+
+def send(connSocket, message, noEncoding = False):
+	if noEncoding:
+		connSocket.send(message)
+	else:
+		connSocket.send(message.encode('utf-8'))
+
+def recive(connSocket):
+	return connSocket.recv(2048).decode('utf-8')
+
+def isReady(connSocket):
+	data = recive(connSocket)
+	if data == "RDY":
+		return True
+	else:
+		return False
+
+def listfiles(connSocket):
+	send(connSocket, 'ALS')
+	filelist = os.listdir('files')
+	for filename in filelist:
+		timeout = 2000
+		while not isReady(connSocket) and timeout > 0:
+			timeout -= 1
+		send(connSocket, filename)
+	send(connSocket, 'END')
+
+def sendfile(connSocket, filename):
+	filedir = 'files/'+filename
+	filesize = os.path.getsize(filedir)
+	with open(filedir, 'rb') as f:
+		send(connSocket, 'ASF')
+		timeout = 2000
+		while not isReady(connSocket) and timeout > 0:
+			timeout -= 1
+		send(connSocket, filename)
+		while not isReady(connSocket) and timeout > 0:
+			timeout -= 1
+		send(connSocket, str(filesize))
+		data = f.read(512)
+		while data:
+			send(connSocket, data, True)
+			data = f.read(2048)
+
+def authorize(connSocket):  #Placeholder
+	send(connSocket, "AUT")
+	data = recive(connSocket)
+	password = "LOL123"
+	if data == password:
+		send(connSocket, "ACK")
+		return True
+	else:
+		send(connSocket, "ERR")
+		return False
+
+def handler(connSocket):
+	if authorize(connSocket):
+		while True:
+			command = recive(connSocket)
+			if not command:
+				break
+			print(command)
+			if command[:3] == 'LST':
+				listfiles(connSocket)
+			elif command[:3] == 'GET':
+				sendfile(connSocket, command[4:])
+			else:
+				pass
+	connSocket.close()
+
+def main():
+	host = '10.0.0.3'
+	port = 5000
+
+	listenSocket = socket.socket()
+	listenSocket.bind((host,port))
+
+	while True:
+		listenSocket.listen(5)
+		connSocket, connAddres = listenSocket.accept()
+		print("Connection"+str(connAddres))
+		handler(connSocket)
+		#Thread(target=handler, args=(connSocket)).start()
+
+if __name__=='__main__':
+	main()
